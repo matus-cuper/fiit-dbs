74191e77c770be813b0bbc5188c42c739878f507
Matus Cuper <matus.cuper@gmail.com>
2017-04-07 10:03:17 +0200

    Remove CTE from main query

--- doc/queries/main.sql
+++ doc/queries/main.sql
@@ -1,38 +1,32 @@
-WITH avg_gss AS
-	(
-	SELECT s.student_id,
-		CASE WHEN ROUND(AVG(gss.mark), 2) IS NULL THEN 5.00
-		ELSE ROUND(AVG(gss.mark), 2) END AS avg
-	FROM students s
-	JOIN graduations_from_ss gss ON s.student_id = gss.student_id
-	GROUP BY s.student_id
-	),
-count_a AS
-	(
-	SELECT s.student_id, COUNT(*)
-	FROM students s
-	JOIN awards a ON s.student_id = a.student_id
-	GROUP BY s.student_id
-	),
-count_r AS
-	(
-	SELECT s.student_id, COUNT(*)
-	FROM students s
-	JOIN registrations r ON s.student_id = r.student_id
-	GROUP BY s.student_id
-	),
-count_g AS
-	(
-	SELECT s.student_id, COUNT(*),
-		SUM(CASE WHEN g.graduated = TRUE THEN 1 ELSE 0 END) AS count_success
-	FROM students s
-	JOIN graduations g ON s.student_id = g.student_id
-	GROUP BY s.student_id
-	)
-
-SELECT s.name, s.surname, s.birth_at, gss.avg, a.count, r.count, g.count, g.count_success
+SELECT s.name, s.surname, s.birth_at,
+	gss.avg,
+	a.count,
+	r.count,
+	g.count,
+	g.count_success
 FROM students s
-LEFT JOIN avg_gss gss ON s.student_id = gss.student_id
-LEFT JOIN count_a a ON s.student_id = a.student_id
-LEFT JOIN count_r r ON s.student_id = r.student_id
-LEFT JOIN count_g g ON s.student_id = g.student_id
+LEFT JOIN
+	(
+	SELECT gss.student_id, ROUND(AVG(gss.mark), 2) AS avg
+	FROM graduations_from_ss gss
+	GROUP BY gss.student_id
+	) gss ON s.student_id = gss.student_id
+LEFT JOIN
+	(
+	SELECT a.student_id, COUNT(*)
+	FROM awards a
+	GROUP BY a.student_id
+	) a ON s.student_id = a.student_id
+LEFT JOIN
+	(
+	SELECT r.student_id, COUNT(*)
+	FROM registrations r
+	GROUP BY r.student_id
+	) r ON s.student_id = r.student_id
+LEFT JOIN
+	(
+	SELECT g.student_id, COUNT(*),
+		SUM(CASE WHEN g.graduated = TRUE THEN 1 ELSE 0 END) AS count_success
+	FROM graduations g
+	GROUP BY g.student_id
+	) g ON s.student_id = g.student_id
