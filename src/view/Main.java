6e0f003a1385f0ddeae663a7beb1387c5bb300ab
matus-cuper <matus.cuper@gmail.com>
2017-04-08 09:32:56 +0200

    Fix typo

--- src/view/Main.java
+++ src/view/Main.java
@@ -16,8 +16,8 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../view/sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        Parent root = FXMLLoader.load(getClass().getResource("mainPane.fxml"));
+        primaryStage.setTitle("Student database");
         primaryStage.setScene(new Scene(root, 1200, 700));
         primaryStage.show();
     }
