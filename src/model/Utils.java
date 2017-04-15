5aef0fecd984daa7742df24f6dbb337888ef1af1
matus-cuper <matus.cuper@gmail.com>
2017-04-15 14:44:04 +0200

    Improve date conversion

--- src/model/Utils.java
+++ src/model/Utils.java
@@ -2,7 +2,6 @@ package model;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.logging.Logger;
 
 /**
  * Created by Matus Cuper on 14.4.2017.
@@ -11,8 +10,6 @@ import java.util.logging.Logger;
  */
 class Utils {
 
-    private static final Logger LOG = Logger.getLogger(Utils.class.getName());
-
     private static SimpleDateFormat parser = new SimpleDateFormat("yyyy-MM-dd");
 
 
@@ -20,6 +17,6 @@ class Utils {
 
     @SuppressWarnings("deprecation")
     static java.sql.Date parseDate(String date) throws ParseException {
-        return new java.sql.Date(parser.parse(date).getYear(), parser.parse(date).getMonth(), parser.parse(date).getDate());
+        return new java.sql.Date(parser.parse(date).getTime());
     }
 }
